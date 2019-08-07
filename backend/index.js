ac085c8637af1c0b1008d697c9572ce8d2241130
Joni Yrjänä <joniyrjana@gmail.com>
2019-08-07 16:49:53 +0300

    Backend: Read and use configuration variables PORT, PROTOCOL, CERT, and CERT_KEY from .env.

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,28 @@
-const app = require('./app')
+const app    = require('./app')
 const config = require('./utils/config')
-const http = require('http')
+const http   = require('http')
+const https  = require('https')
+const fs     = require('fs')
 
-const server = http.createServer(app)
-server.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
-})
+if (config.PROTOCOL === "http") {
+  const server = http.createServer(app)
+  server.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`)
+  })
+  
+} else if (config.PROTOCOL === "https") {
+  const options = {
+    cert: fs.readFileSync(config.CERT),
+    key:  fs.readFileSync(config.CERT_KEY)
+  }
+  
+  const server = https.createServer(options, app)
+  server.listen(config.PORT, () => {
+    console.log(`Server (SSL) running on port ${config.PORT}`)
+  })
+  
+} else {
+  console.error(`Error, unknown protocol '${config.PROTOCOL}'`)
+}
+
+
--- backend/utils/config.js
+++ backend/utils/config.js
@@ -3,9 +3,15 @@ require('dotenv')
     silent: process.env.NODE_ENV === 'production'
   })
 
-let PORT = 3001
+const PORT     = process.env.PORT     !== undefined ? process.env.PORT :     3001
+const PROTOCOL = process.env.PROTOCOL !== undefined ? process.env.PROTOCOL : "http"
+const CERT     = process.env.CERT     !== undefined ? process.env.CERT :     "cert.pem"
+const CERT_KEY = process.env.CERT_KEY !== undefined ? process.env.CERT_KEY : "key.pem"
 
 
 module.exports = {
-  PORT
+  PORT,
+  PROTOCOL,
+  CERT,
+  CERT_KEY
 }
