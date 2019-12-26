a59ed73973c165ecbad7fcad3c88e190d71ec2bb
Joni Yrjänä <joniyrjana@gmail.com>
2019-12-26 19:59:13 +0200

    Backend: Replace test code with proper code in login controller.

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -2,17 +2,30 @@ const jwt         = require('jsonwebtoken')
 const bcrypt      = require('bcrypt')
 const loginRouter = require('express').Router()
 
+const User = require('../models/user')
+
 loginRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const user = {
-    username: "test",
-    id:       1
-  }
+  const user = await User.findOne({
+    where: {
+      username: body.username
+    }
+  })
   
+  const passwordCorrect = 
+    user === null ? false
+                  : await bcrypt.compare(body.password, user.password_hash)
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
+
   const userForToken = {
     username: user.username,
-    id:       user._id
+    id:       user._id,
   }
 
   const token = jwt.sign(userForToken, process.env.SECRET)
@@ -24,7 +37,7 @@ loginRouter.post('/', async (request, response) => {
     token
   }
 
-  response
+  return response
     .status(200)
     .send(retuser)
 })
